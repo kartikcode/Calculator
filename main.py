855df6b22816b9629741122c538f84ac30463717
kartikcode <59219455+kartikcode@users.noreply.github.com>
2019-12-29 19:21:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+from kivy.app import App
+from kivy.boxlayout import Boxlayout
+from kivy.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
 
+class Calculator(self):
+  
+
+
+
+
+
+
+
+class CalculatorApp(self):
+  def build(self):
+    pass
+
+if __name__="__main":
+  CalculatorApp().run()
